3cbbb0e8f715018b8c1152cb801ec4ccd46bdf5a
dinhduycity <duyten321@gmail.com>
2024-12-05 14:03:40 +0700

    Add files via upload

---
+++ bai 3/3.6.py
@@ -0,0 +1,10 @@
+print (" le dinh duy")
+print ("235752021610087")
+
+def get_sum(*num):
+    tmp=0
+    for i in num:
+        tmp += i
+    return tmp
+result=get_sum(1, 2, 3, 4, 5)
+print(result)
